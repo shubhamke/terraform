50e1757979834af6644e664d09add82ca8492131
shubhamke <149041790+shubhamke@users.noreply.github.com>
2024-01-08 10:36:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ provider "aws" {
 access_key = var.AWS_ACCESS_KEY
 secret_key = var.AWS_SECRET_KEY
 region = "us-east-1"
-} resource "aws_instance" "myec2" {
+}
+resource "aws_instance" "myec2" {
   ami           = "ami-079db87dc4c10ac91"
   instance_type = "t2.micro"
   vpc_security_group_ids=[aws_security_group.web-sg.id]
